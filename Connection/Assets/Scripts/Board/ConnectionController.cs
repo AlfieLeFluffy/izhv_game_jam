32cc5510a3d40ded267869469166a72e6f8c9ed9
Bára Šopfová <bara.sopfova@seznam.cz>
2024-01-19 20:13:28 +0100

    Added side bar in board scene

--- Connection/Assets/Scripts/Board/ConnectionController.cs
+++ Connection/Assets/Scripts/Board/ConnectionController.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConnectionController : MonoBehaviour
 {
     public Transform[] points;
-
     public GameObject linePrefab;
-    private GameObject newLine;
+    public GameObject postedNotePrefab;
+    public GameObject unusedNotePrefab;
+
+    public Transform lines;
+    public Transform postedNotes;
+    public Transform unusedNotes;
+
+    private GameObject newObject;
     
     
     void Start()
     {
-        newLine = Instantiate(linePrefab);
-        LineController lc = newLine.GetComponent<LineController>();
+
+        CreateConnection();
+    }
+
+
+    public void CreateConnection()
+    {
+        newObject = Instantiate(linePrefab, lines);
+
+        LineController lc = newObject.GetComponent<LineController>();
         lc.pointA = points[0];
         lc.pointB = points[1];
+    }
+
+    public void AddNewNote()
+    {
+        newObject = Instantiate(unusedNotePrefab, unusedNotes);
+        newObject.GetComponent<RectTransform>().localPosition = new Vector3(unusedNotes.childCount * 60 - 10, 0, 0);
     }
 }
