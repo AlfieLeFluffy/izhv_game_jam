5657c86c08c34d761b28beae388bff9c2de43681
Bára Šopfová <bara.sopfova@seznam.cz>
2024-01-21 16:00:56 +0100

    Deleting connections

--- Connection/Assets/Scripts/Board/CursorScript.cs
+++ Connection/Assets/Scripts/Board/CursorScript.cs
@@ -12,6 +12,7 @@ public class CursorScript : MonoBehaviour
 
     private GameObject col;
     private Vector3 start;
+    private LineRenderer l;
 
 
     private void Start()
@@ -27,18 +28,34 @@ public class CursorScript : MonoBehaviour
             {
                 start = this.transform.position;
             }
+
             if (Input.GetMouseButton(0))
             {
                 PostedNoteMove(col.transform);
             }
+
             if (Input.GetMouseButtonUp(0))
             {
-                PostedNoteDrop(col.transform);
+                if (col.TryGetComponent<LineRenderer>(out l))
+                {
+                    ChangeConnectionText(col.transform);
+                }
+                else
+                {
+                    PostedNoteDrop(col.transform);
+                }
             }
 
-            else if (Input.GetMouseButtonUp(1))
+            if (Input.GetMouseButtonUp(1))
             {
-                PostedNoteInteraction(col.transform);
+                if (col.TryGetComponent<LineRenderer>(out l))
+                {
+                    DeleteConnection(col.transform);
+                }
+                else
+                {
+                    PostedNoteInteraction(col.transform);
+                }
             }
         }
 
@@ -58,6 +75,19 @@ public class CursorScript : MonoBehaviour
         }
     }
 
+
+    private void ChangeConnectionText(Transform line)
+    {
+        print("yay");
+    }
+
+    private void DeleteConnection(Transform line)
+    {
+        if (controller.connect)
+        { 
+            Destroy(line.gameObject);
+        }
+    }
 
     public void PostedNoteMove(Transform note)
     {
@@ -81,13 +111,14 @@ public class CursorScript : MonoBehaviour
         {
             if (lines.childCount != 0)
             {
-                if (lines.GetChild(lines.childCount - 1).GetComponent<LineController>().pointA == note)
+                if (lines.GetChild(lines.childCount - 1).GetComponent<LineController>().pointB == this.transform)
                 {
-                    Destroy(lines.GetChild(lines.childCount - 1).gameObject);
-                    return;
-                }
-                else if (lines.GetChild(lines.childCount - 1).GetComponent<LineController>().pointB == this.transform)
-                {
+                    if (lines.GetChild(lines.childCount - 1).GetComponent<LineController>().pointA == note)
+                    {
+                        Destroy(lines.GetChild(lines.childCount - 1).gameObject);
+                        return;
+                    }
+                    
                     lines.GetChild(lines.childCount - 1).GetComponent<LineController>().pointB = note;
                     return;
                 }
--- Connection/Assets/Scripts/Board/LineController.cs
+++ Connection/Assets/Scripts/Board/LineController.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class LineController : MonoBehaviour
 {
     public Transform pointA;
     public Transform pointB;
 
-    private LineRenderer lr;
+    private LineRenderer line;
+    private Vector3 v;
     
     
     void Start()
     {
-        lr = GetComponent<LineRenderer>();
+        line = GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        lr.SetPosition(0, pointA.position);
-        lr.SetPosition(1, pointB.position);
+        line.SetPosition(0, pointA.position);
+        line.SetPosition(1, pointB.position);
+
+        this.transform.position = pointA.position + (pointB.position - pointA.position) / 2;
+        this.GetComponent<CapsuleCollider>().height = Vector3.Distance(pointA.position, pointB.position) / 10 - 2;
+
+        v = pointB.position - pointA.position;
+        this.transform.rotation = Quaternion.Euler(0, 0, Vector3.Angle(v, (v.x < 0) ? Vector3.up : Vector3.down));
     }
 }
